258b0b72784f3e62997d3984d142aaf20f4d9ffe
Stefan <git@stefn.co.uk>
2024-12-17 15:54:30 +0000

    Update API key

--- flake.nix
+++ flake.nix
@@ -1,57 +1,70 @@
 {
-
   description = "A best script!";
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
   outputs =
     {
       self,
       nixpkgs,
       flake-utils,
     }:
-
     flake-utils.lib.eachDefaultSystem (
       system:
-
       let
-        pkgs = import nixpkgs { inherit system; };
-        serveName = "serve";
-        pushName = "push";
-        buildInputs = with pkgs; [
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        # Common build inputs
+        commonBuildInputs = with pkgs; [
           ruby_3_2
-          rubyPackages_3_2.minima
-          jekyll
           surge-cli
         ];
-        serveScript = (pkgs.writeScriptBin serveName (builtins.readFile ./bin/serve)).overrideAttrs (old: {
-          buildCommand = "${old.buildCommand}\n patchShebangs $out";
-        });
-        pushScript = (pkgs.writeScriptBin pushName (builtins.readFile ./bin/push)).overrideAttrs (old: {
-          buildCommand = "${old.buildCommand}\n patchShebangs $out";
-        });
+
+        # Helper function to create scripts
+        mkScript =
+          name:
+          (pkgs.writeScriptBin name (builtins.readFile ./bin/${name})).overrideAttrs (old: {
+            buildCommand = "${old.buildCommand}\n patchShebangs $out";
+          });
+
+        # Helper function to create packages
+        mkPackage =
+          name:
+          pkgs.symlinkJoin {
+            inherit name;
+            paths = [ (mkScript name) ] ++ commonBuildInputs;
+            buildInputs = [ pkgs.makeWrapper ];
+            postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+          };
+
+        # Script names
+        scripts = [
+          "build"
+          "serve"
+          "push"
+        ];
+
+        # Generate all packages
+        scriptPackages = builtins.listToAttrs (
+          map (name: {
+            inherit name;
+            value = mkPackage name;
+          }) scripts
+        );
+
       in
       rec {
-        defaultPackage = packages.serveScript;
-        packages.serveScript = pkgs.symlinkJoin {
-          name = serveName;
-          paths = [ serveScript ] ++ buildInputs;
-          buildInputs = [ pkgs.makeWrapper ];
-          postBuild = "wrapProgram $out/bin/${serveName} --prefix PATH : $out/bin";
-        };
-        packages.pushScript = pkgs.symlinkJoin {
-          name = pushName;
-          paths = [ pushScript ] ++ buildInputs;
-          buildInputs = [ pkgs.makeWrapper ];
-          postBuild = "wrapProgram $out/bin/${pushName} --prefix PATH : $out/bin";
-        };
-        devShell = pkgs.mkShell {
-          buildInputs = buildInputs ++ [
-            packages.serveScript
-            packages.pushScript
-          ];
+        defaultPackage = packages.serve;
+        packages = scriptPackages;
+
+        devShells = rec {
+          default = dev;
+          dev = pkgs.mkShell {
+            buildInputs = commonBuildInputs ++ (builtins.attrValues packages);
+          };
         };
       }
     );
-
 }
